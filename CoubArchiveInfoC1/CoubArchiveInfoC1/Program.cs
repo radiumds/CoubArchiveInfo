ff40ff55fc57190c26a8a7876540d6db68a99297
moonstyle <radiumds@gmail.com>
2022-03-20 21:46:22 +0300

    Configuration NLog, first

--- CoubArchiveInfoC1/CoubArchiveInfoC1/Program.cs
+++ CoubArchiveInfoC1/CoubArchiveInfoC1/Program.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
+using NLog;
 
 namespace CoubArchiveInfoC1
 {
     class Program
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            try
+            {
+                Logger.Info("Hello world");
+                System.Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "Goodbye cruel world");
+
+
+            }
         }
     }
 }
