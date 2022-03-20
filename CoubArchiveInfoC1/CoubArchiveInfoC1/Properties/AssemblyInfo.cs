774263b110eb994503088a6f74419100ab3f4419
moonstyle <radiumds@gmail.com>
2022-03-20 20:29:41 +0300

    Change Type of project from winForm to Console Same add NuGet Package sqlite

--- CoubArchiveInfoC1/CoubArchiveInfoC1/Properties/AssemblyInfo.cs
+++ CoubArchiveInfoC1/CoubArchiveInfoC1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("CoubArchiveInfoC1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("CoubArchiveInfoC1")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("1285d6c8-5bc6-476c-8c0a-7c837c29875e")]
+[assembly: Guid("24be4534-1f88-43d2-aea6-49c722183c45")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
